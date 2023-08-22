9ef8ae13bec44373182756dc2d72644a47a70535
Oleg Sazonov <whitylmn@gmail.com>
2023-08-22 14:28:36 +0300

    Shrink image to 13M

--- app/main.py
+++ app/main.py
@@ -1,32 +1,38 @@
-from flask import Flask,abort
+from http.server import BaseHTTPRequestHandler, HTTPServer
 from socket import gethostname
 from os import getenv
+from functools import cached_property
+from urllib.parse import urlparse
 
-app = Flask(__name__)
-
-@app.route("/hostname", methods=['GET'])
-def index_hostname():
-    hostname = gethostname()
-    if hostname == None:
-        abort(404, "Failed to get hostname")
-    else:
-        return hostname
-
-@app.route("/author", methods=['GET'])
-def index_author():
-    author = getenv("AUTHOR")
-    if author == None:
-        abort(404, "Failed to get author!")
-    else:
-        return author
-
-@app.route("/id", methods=['GET'])
-def index_uuid():
-    uuid = getenv("UUID")
-    if uuid == None:
-        abort(404, "Failed to get UUID!")
-    else:
-        return uuid
-
+class WebRequestHandler(BaseHTTPRequestHandler):
+    @cached_property
+    def url(self):
+        return urlparse(self.path)
+    
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        output = ""
+        match self.url.path:
+            case "/hostname":
+                output = gethostname()
+            case "/id":
+                output = getenv("UUID")
+                if output == None:
+                    self.wfile.write("Failed to get UUID!".encode())
+                    return
+            case "/author":
+                output = getenv("AUTHOR")
+                if output == None:
+                    self.wfile.write("Failed to get author!".encode())
+                    return
+            case _:
+                self.wfile.write("You hit wrong path!".encode())
+                return
+        self.wfile.write(output.encode())
+        return
+        
+        
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    server = HTTPServer(("0.0.0.0",8000), WebRequestHandler)
+    server.serve_forever()
